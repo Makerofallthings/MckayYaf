3bde61fab475d2ed5bc3b00277e274662e946ca3
Your Name <you@example.com>
2026-01-30 19:42:44 -0800

    initial commit

---
+++ src/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navigation from '@/components/Navigation';
+import Footer from '@/components/Footer';
+
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen bg-slate-900 flex flex-col">
+      <Navigation />
+      <main className="flex-1">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
